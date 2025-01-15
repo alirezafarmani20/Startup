d9bb5b8338da23bba9062618ccaba082ef8e15fc
alireza <farmanialireza668@gmail.com>
2025-01-15 16:43:44 +0330

    create works components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import MainBaner from '@/components/MainBaner.vue'
 import Brands from '@/components/Brands.vue'
 import Conections from '@/components/Conections.vue';
+import Works from '@/components/Works.vue';
 </script>
 
 <template>
@@ -9,12 +10,16 @@ import Conections from '@/components/Conections.vue';
     <!--baner section-->
     <MainBaner />
   </div>
-  <div>
+  <div class=" my-3">
     <!--brands section-->
     <Brands />
   </div>
-  <div>
+  <div class="my-3">
     <!--conections section-->
     <Conections />
+  </div>
+  <div class="my-3">
+    <!--works section-->
+    <Works />
   </div>
 </template>
