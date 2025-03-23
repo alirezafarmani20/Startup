c8e0b679939663e5b0b631f602f21d9b6dab65e9
alireza <farmanialireza668@gmail.com>
2025-03-24 00:36:06 +0330

    update tailwindcss

--- src/components/Forme.vue
+++ src/components/Forme.vue
@@ -53,7 +53,7 @@ import Button from './Button.vue'
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345093747!2d144.9537363153169!3d-37.8162797797517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d8b1f9b1b1b1!2sFederation%20Square!5e0!3m2!1sen!2sau!4v1611816751234!5m2!1sen!2sau"
             allowfullscreen=""
             loading="lazy"
-            class="my-3 w-full sm:h-[385px] rounded"
+            class="my-3 w-full sm:h-[385px] rounded-sm"
           ></iframe>
         </div>
       </div>
